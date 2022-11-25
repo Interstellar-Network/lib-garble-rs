c5c0ac711d0667a3e525f539840e323b417b6007
Nathan Prat <nathan.prat@gmail.com>
2022-11-25 11:46:13 +0100

    WIP skcd: use new inputs order

--- examples/classic.rs
+++ examples/classic.rs
@@ -16,7 +16,7 @@ fn main() {
 
     // TODO(interstellar) display_message_640x360_2digits.skcd.pb.bin
     let f =
-        std::fs::File::open("examples/data/display_message_120x52_2digits.skcd.pb.bin").unwrap();
+        std::fs::File::open("examples/data/display_message_640x360_2digits.skcd.pb.bin").unwrap();
     let mut reader = BufReader::new(f);
 
     let mut buffer = Vec::new();
@@ -28,10 +28,10 @@ fn main() {
     let mut garb = InterstellarGarbledCircuit::garble(circ);
 
     // TODO(interstellar) get from Circuit's "config"
-    // let width = 640;
-    // let height = 360;
-    let width = 120;
-    let height = 52;
+    let width = 640;
+    let height = 360;
+    // let width = 120;
+    // let height = 52;
 
     let mut merged_outputs = vec![0u16; width * height];
     let mut rng = thread_rng();
@@ -40,21 +40,21 @@ fn main() {
     for _ in 0..NB_EVALS {
         // TODO(interstellar)!!! BROKEN CIRCUIT??? no way to properly display a given digit?
         let mut evaluator_inputs = vec![
+            // cf Verilog "rndswitch.v"; and correspondingly lib_garble/src/packmsg/packmsg_utils.cpp PrepareInputLabels
+            // It MUST always be 0; else the segments will not be displayed properly
+            0, //
             // first digit: 7 segments: 4
             0, 1, 1, 1, 0, 1, 0, //
             // second digit: 7 segments: 2
             1, 0, 1, 1, 1, 0, 1, //
             // "rnd": 10 inputs; value SHOULD not really matter for this test???
             0, 0, 0, 0, 0, 0, 0, 0, 0, //
-            // cf Verilog "rndswitch.v"; and correspondingly lib_garble/src/packmsg/packmsg_utils.cpp PrepareInputLabels
-            // It MUST always be 0; else the segments will not be displayed properly
-            0,
         ];
 
         // randomize the "rnd" part of the inputs
         let size = evaluator_inputs.len();
         // cf "rndswitch.v" comment above; DO NOT touch the last!
-        for input in evaluator_inputs[14..size - 1].iter_mut() {
+        for input in evaluator_inputs[14 + 1..size].iter_mut() {
             *input = rand_0_1.sample(&mut rng);
         }
 

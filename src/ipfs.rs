a48a6141c1610cf25eabd0bf3390264086ae73b1
Nathan Prat <nathan.prat@gmail.com>
2022-12-16 14:18:20 +0100

    WIP ipfs [7]
    refactor into "pub struct IpfsClient"

--- src/ipfs.rs
+++ src/ipfs.rs
@@ -27,6 +27,10 @@ pub enum IpfsError {
     ResponseError { err: http_req_error::Error },
     #[snafu(display("http error[{}]: {}", code, msg))]
     HttpError { msg: String, code: u16 },
+    #[snafu(display("uri error: {}", msg))]
+    UriError { msg: String },
+    #[snafu(display("tcp stream error: {}", msg))]
+    TcpStreamError { msg: String },
 }
 
 type Result<T, E = IpfsError> = std::result::Result<T, E>;
@@ -58,87 +62,131 @@ pub const MULTIPART_CONTENT_DISPOSITION: &[u8] =
     b"Content-Disposition: form-data; name=\"file\"; filename=\"TODO_path\"";
 pub const MULTIPART_CONTENT_TYPE: &[u8] = b"Content-Type: application/octet-stream";
 
-/// IPFS add
-/// cf https://docs.ipfs.tech/reference/kubo/rpc/#api-v0-add
-/// and https://github.com/ferristseng/rust-ipfs-api/blob/master/ipfs-api-prelude/src/request/add.rs
-///
-/// param root_uri: eg "http://localhost:5001"
-pub fn ipfs_add(root_uri: &str, body: &[u8]) -> Result<IpfsAddResponse, IpfsError> {
-    let uri = format!("{}{}/add", root_uri, VERSION_PATH_V0);
-    let uri: &str = &uri;
+pub struct IpfsClient {
+    // This is NOT a Uri b/c it would require keep a ref to the underlying &str; ie Uri<'a>
+    root_uri: String,
+    stream: TcpStream,
+    // Container for response's body
+    // TODO(interstellar) move under send_request or new_request[ie make it Send/Sync]
+    writer: Vec<u8>,
+}
 
+fn parse_uri<'a>(uri_str: &'a str) -> Result<Uri<'a>, IpfsError> {
     // Parse uri and assign it to variable `addr`
     // TODO(interstellar) why do we get "the trait `FromStr` is not implemented for `Uri<'_>`" in either SGX or STD???
     #[cfg(feature = "std")]
-    let addr: Uri = Uri::try_from(uri).map_err(|err| IpfsError::ResponseError { err: err })?;
+    let addr: Uri = Uri::try_from(uri_str).map_err(|err| IpfsError::UriError {
+        msg: format!("invalid uri: {}", uri_str),
+    })?;
     #[cfg(all(not(feature = "std"), feature = "sgx"))]
-    let addr: Uri = uri.parse().unwrap();
+    let addr: Uri = uri_str.parse()?;
 
-    //Connect to remote host
-    let mut stream = TcpStream::connect((addr.host().unwrap(), addr.corr_port())).unwrap();
+    Ok(addr)
+}
 
-    // Open secure connection over TlsStream, because of `addr` (https)
-    // TODO(interstellar) IPFS support https
-    // let mut stream = tls::Config::default()
-    //     .connect(addr.host().unwrap_or(""), stream)
-    //     .unwrap();
+impl IpfsClient {
+    pub fn new(root_uri: &str) -> Result<Self> {
+        let api_uri = format!("{}{}", root_uri, VERSION_PATH_V0);
+        let addr = parse_uri(&api_uri)?;
 
-    //Container for response's body
-    let mut writer = Vec::new();
+        //Connect to remote host
+        let stream = TcpStream::connect((
+            addr.host().ok_or_else(|| IpfsError::UriError {
+                msg: format!("invalid host: {}", addr),
+            })?,
+            addr.corr_port(),
+        ))
+        .map_err(|err| IpfsError::TcpStreamError {
+            msg: err.to_string(),
+        })?;
 
-    // TODO(interstellar) avoid copying
-    let multipart_start = [
-        MULTIPART_BOUNDARY,
-        MULTIPART_NEW_LINE,
-        MULTIPART_CONTENT_DISPOSITION,
-        MULTIPART_NEW_LINE,
-        MULTIPART_CONTENT_TYPE,
-        MULTIPART_NEW_LINE,
-        // Space b/w "headers" and "body"
-        MULTIPART_NEW_LINE,
-    ]
-    .concat();
-    // No need for a new line at the end
-    let body_bytes = [
-        multipart_start.as_slice(),
-        body,
-        MULTIPART_NEW_LINE,
-        MULTIPART_BOUNDARY,
-        b"--",
-    ]
-    .concat();
+        // Open secure connection over TlsStream, because of `addr` (https)
+        // TODO(interstellar) IPFS support https
+        // let mut stream = tls::Config::default()
+        //     .connect(addr.host().unwrap_or(""), stream)
+        //     .unwrap();
 
-    // TODO(interstellar)???
-    // let body_bytes = body_bytes.replace("\n", "\r\n");
-    // .header("Content-Type", "application/octet-stream")
-    //Add header `Connection: Close`
-    let mut request: RequestBuilder = RequestBuilder::new(&addr);
-    request.timeout(Some(Duration::from_millis(1000)));
-    // TODO(interstellar) keep-alive? is it needed?
-    // TODO(interstellar)
-    request.header("Content-Type", "multipart/form-data;boundary=\"boundary\"");
-    request.header("Content-Length", &body_bytes.len().to_string());
-    request.method(Method::POST);
-    // TODO(interstellar)
-    request.body(&body_bytes);
-    let result = request.send(&mut stream, &mut writer);
+        Ok(IpfsClient {
+            root_uri: api_uri,
+            stream,
+            writer: Vec::new(),
+        })
+    }
 
-    // println!("{}", String::from_utf8_lossy(&writer));
-    // println!("Status: {} {}", response.status_code(), response.reason());
+    /// IPFS add
+    /// cf https://docs.ipfs.tech/reference/kubo/rpc/#api-v0-add
+    /// and https://github.com/ferristseng/rust-ipfs-api/blob/master/ipfs-api-prelude/src/request/add.rs
+    ///
+    /// param root_uri: eg "http://localhost:5001"
+    pub fn ipfs_add(&mut self, body: &[u8]) -> Result<IpfsAddResponse, IpfsError> {
+        // TODO(interstellar) avoid copying
+        let multipart_start = [
+            MULTIPART_BOUNDARY,
+            MULTIPART_NEW_LINE,
+            MULTIPART_CONTENT_DISPOSITION,
+            MULTIPART_NEW_LINE,
+            MULTIPART_CONTENT_TYPE,
+            MULTIPART_NEW_LINE,
+            // Space b/w "headers" and "body"
+            MULTIPART_NEW_LINE,
+        ]
+        .concat();
+        // No need for a new line at the end
+        let body_bytes = [
+            multipart_start.as_slice(),
+            body,
+            MULTIPART_NEW_LINE,
+            MULTIPART_BOUNDARY,
+            b"--",
+        ]
+        .concat();
+
+        let full_uri_str = format!("{}{}", self.root_uri, "/add");
+        let full_uri = parse_uri(&full_uri_str)?;
+        let mut request = new_request(&full_uri)?;
+        request.header("Content-Type", "multipart/form-data;boundary=\"boundary\"");
+        request.header("Content-Length", &body_bytes.len().to_string());
+        // TODO(interstellar)
+        request.body(&body_bytes);
+
+        send_request(&mut self.stream, &mut self.writer, request)
+    }
+
+    /// https://docs.ipfs.tech/reference/kubo/rpc/#api-v0-cat
+    pub fn ipfs_cat() {}
+}
+
+fn send_request<'a, ResponseType: Deserialize<'a>>(
+    stream: &mut TcpStream,
+    writer: &'a mut Vec<u8>,
+    request: RequestBuilder,
+) -> Result<ResponseType, IpfsError> {
+    let result = request.send(stream, writer);
 
     match result {
         Ok(response) => {
             let status_code = response.status_code();
             if status_code.is_success() {
-                let add_response: IpfsAddResponse = serde_json::from_slice(&writer).unwrap();
+                let add_response: ResponseType = serde_json::from_slice(writer).unwrap();
                 Ok(add_response)
             } else {
                 Err(IpfsError::HttpError {
-                    msg: String::from_utf8(writer).unwrap(),
+                    // TODO(interstellar) remove clone
+                    msg: String::from_utf8(writer.clone()).unwrap(),
                     code: u16::from(response.status_code()),
                 })
             }
         }
         Err(err) => Err(IpfsError::ResponseError { err: err }),
     }
+}
+
+fn new_request<'a>(full_uri: &'a Uri) -> Result<RequestBuilder<'a>> {
+    // TODO(interstellar) keep-alive? is it needed? or Close?
+    let mut request: RequestBuilder = RequestBuilder::new(full_uri);
+    // TODO(interstellar) timeout from new()
+    request.timeout(Some(Duration::from_millis(1000)));
+    request.method(Method::POST);
+
+    Ok(request)
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,17 +1,18 @@
 mod common;
 use ipfs_api_backend_hyper::IpfsApi;
-use lib_garble_rs::ipfs::ipfs_add;
+use lib_garble_rs::ipfs::IpfsClient;
 use libp2p::futures::TryStreamExt;
 
 #[test]
 fn test_ipfs_add() {
-    let (foreign_node, ipfs_client) = common::foreign_ipfs::run_ipfs_in_background();
+    let (foreign_node, ipfs_server_client) = common::foreign_ipfs::run_ipfs_in_background();
     // let ipfs_server_multiaddr = format!("/ip4/127.0.0.1/tcp/{}", foreign_node.api_port);
     let ipfs_server_multiaddr = format!("http://localhost:{}", foreign_node.api_port);
 
     // AZaz
     let content = &[65u8, 90, 97, 122];
-    let res = ipfs_add(&ipfs_server_multiaddr, content);
+    let mut ipfs_client = IpfsClient::new(&ipfs_server_multiaddr).unwrap();
+    let res = ipfs_client.ipfs_add(content);
 
     assert!(res.is_ok());
 
@@ -19,7 +20,7 @@ fn test_ipfs_add() {
 
     // Compare using the official client; API call = IPFS cat
     let skcd_buf = tokio_test::block_on({
-        ipfs_client
+        ipfs_server_client
             .cat(&add_response.hash)
             .map_ok(|chunk| chunk.to_vec())
             .try_concat()
